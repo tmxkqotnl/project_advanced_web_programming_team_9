ca4e0dbe0ea41c09bbbc0f1c37932d33c3b90ad7
rkdtjdwls177 <71641851+rkdtjdwls177@users.noreply.github.com>
2021-06-05 19:57:36 +0900

    Add files via upload

--- public/data/covid19.js
+++ public/data/covid19.js
@@ -4,67 +4,110 @@ const fs = require("fs");
 
 // 데이터 출처 : https://www.data.go.kr/data/15043378/openapi.do
 
-  let today = new Date();
-  let year = today.getFullYear();
-  let month = today.getMonth() + 1;
-  let day = today.getDate();
-  let date = `${year}0${month}${day}`;
+let today = new Date();
+let year = today.getFullYear();
+let month  = ("0" + (1 + today.getMonth())).slice(-2);
+let day = ("0" + today.getDate()).slice(-2);
+let date = `${year}${month}${day}`;
 
-  console.log(`${date}`);
-  const url =
-    "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson";
-  var queryParams =
-    "?" +
-    encodeURIComponent("ServiceKey") +
-    "=vdV%2B8EeWPgBthUe3Y9mji7ct6VbJO7y8ShTDPLDoQKis7tw%2BIofWzd4tAQHHEADRnLTFW3a0HCDYopMs3ZRLvg%3D%3D"; /* Service Key*/
-  queryParams +=
-    "&" + encodeURIComponent("pageNo") + "=" + encodeURIComponent("1"); /* */
-  queryParams +=
-    "&" +
-    encodeURIComponent("numOfRows") +
-    "=" +
-    encodeURIComponent("10"); /* */
-  queryParams +=
-    "&" +
-    encodeURIComponent("startCreateDt") +
-    "=" +
-    encodeURIComponent(date); /* */
-  queryParams +=
-    "&" +
-    encodeURIComponent("endCreateDt") +
-    "=" +
-    encodeURIComponent(date); /* */
+console.log(`${date}`);
+const url =
+  "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson";
+var queryParams =
+  "?" +
+  encodeURIComponent("ServiceKey") +
+  "=vdV%2B8EeWPgBthUe3Y9mji7ct6VbJO7y8ShTDPLDoQKis7tw%2BIofWzd4tAQHHEADRnLTFW3a0HCDYopMs3ZRLvg%3D%3D"; /* Service Key*/
+queryParams +=
+  "&" + encodeURIComponent("pageNo") + "=" + encodeURIComponent("1"); /* */
+queryParams +=
+  "&" + encodeURIComponent("numOfRows") + "=" + encodeURIComponent("10"); /* */
+queryParams +=
+  "&" +
+  encodeURIComponent("startCreateDt") +
+  "=" +
+  encodeURIComponent(date); /* */
+queryParams +=
+  "&" +
+  encodeURIComponent("endCreateDt") +
+  "=" +
+  encodeURIComponent(date); /* */
 
-  let GUBUN = [];
-  let incDec = [];
+let GUBUN = [];
+let incDec = [];
 
-  request(
-    {
-      url: url + queryParams,
-      method: "GET",
-    },
-    function (err, res, body) {
-      if (err) {
-        console.log(`ERROR`);
-      } else {
-        var cdata= JSON.stringify(body);
-
-        const $ = cheerio.load(body);
-
-        $("item").each(function (idx) {
-          GUBUN.push($(this).find("GUBUN").text());
-          incDec.push($(this).find("incDec").text());
-        });
-        var file = fs.createWriteStream('covid19data.js');
-        file.on('error', function(err) { Console.log(err) });
-        file.write('var covid19=[');
-        incDec.forEach(value => file.write(`${value},\n`));
-        file.write(']');
-        file.end();
-        for (var i = 0, ii = GUBUN.length; i < ii; i++) {
-          // console.log(`지역 : ${GUBUN[i]} 확진자수 :${incDec[i]}`);
-        }
+request(
+  {
+    url: url + queryParams,
+    method: "GET",
+  },
+  function (err, res, body) {
+    if (err) {
+      console.log(`ERROR`);
+    } else {
+      const $ = cheerio.load(body);
+      $("item").each(function (idx) {
+        GUBUN.push($(this).find("GUBUN").text());
+        incDec.push($(this).find("incDec").text());
+      });
+      var file = fs.createWriteStream("public/data/covid19data.js");
+      file.on("error", function (err) {
+        console.log(err);
+      });
+      file.write("var covid19=[");
+      incDec.forEach((value) => file.write(`${value},\n`));
+      file.write("]");
+      file.end();
+      if (incDec.length == 0) {
+        var queryParams =
+          "?" +
+          encodeURIComponent("ServiceKey") +
+          "=vdV%2B8EeWPgBthUe3Y9mji7ct6VbJO7y8ShTDPLDoQKis7tw%2BIofWzd4tAQHHEADRnLTFW3a0HCDYopMs3ZRLvg%3D%3D"; /* Service Key*/
+        queryParams +=
+          "&" +
+          encodeURIComponent("pageNo") +
+          "=" +
+          encodeURIComponent("1"); /* */
+        queryParams +=
+          "&" +
+          encodeURIComponent("numOfRows") +
+          "=" +
+          encodeURIComponent("10"); /* */
+        queryParams +=
+          "&" +
+          encodeURIComponent("startCreateDt") +
+          "=" +
+          encodeURIComponent(date - 1); /* */
+        queryParams +=
+          "&" +
+          encodeURIComponent("endCreateDt") +
+          "=" +
+          encodeURIComponent(date); /* */
+        request(
+          {
+            url: url + queryParams,
+            method: "GET",
+          },
+          function (err, res, body) {
+            if (err) {
+              console.log(`ERROR`);
+            } else {
+              const $ = cheerio.load(body);
+              $("item").each(function (idx) {
+                GUBUN.push($(this).find("GUBUN").text());
+                incDec.push($(this).find("incDec").text());
+              });
+              var file = fs.createWriteStream("public/data/covid19data.js");
+              file.on("error", function (err) {
+                console.log(err);
+              });
+              file.write("var covid19=[");
+              incDec.forEach((value) => file.write(`${value},\n`));
+              file.write("]");
+              file.end();
+            }
+          }
+        );
       }
     }
-  );
-
+  }
+);
